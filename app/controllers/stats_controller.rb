3dd96d7307231867b4bb7e3439c948c83e25403a
Megan Wenzinger <forlornlawngnome@gmail.com>
2014-10-14 22:25:15 -0400

    changed graph to do each school individually

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,5 +1,50 @@
 class StatsController < ApplicationController
   def hours
+    calculateHours2
+    
+    schoolArray
+    @chartString = "['Date'"
+    @schoolArray.each do |school|
+      @chartString = "#{@chartString},'#{school}'" 
+    end
+    @chartString = "#{@chartString},'All'],".html_safe
+  end
+  def schoolArray
+    @schoolArray = []
+    School.order(:name).each do |school|
+      @schoolArray.push(school.name)
+    end
+    @schoolArray.push("Other Students")
+  end
+  def calculateHours2
+    ismentor = School.where("lower(name) = ?","mentor").first
+    @allHours = Hash.new{|h,k| h[k] = {}}
+    
+    studentlogs = Timelog.where("timein >= ? ", ApplicationHelper.getStartDate).order("timein ASC")
+    studentlogs = studentlogs.group_by{|a| a.timein.at_beginning_of_week}
+    studentlogs.each do |log|
+      totalsum=0
+      
+      schoolHours = Hash.new{|h,k| h[k] = []}
+      schools = log[1].group_by{|a|a.user.school}
+
+      schools.each do |school|
+        sum=0
+        school[1].each do |timelog|
+          totalsum = totalsum+timelog.time_logged
+          sum = sum + timelog.time_logged
+        end
+        
+        sum=sum/(60*60) #Convert from seconds to hours for the graph
+        schoolHours[school[0].name] << sum rescue schoolHours["Other Students"] << sum
+      end
+      totalsum=totalsum/(60*60) #Convert from seconds to hours for the graph
+      
+      @allHours[log[0].strftime("%m/%d/%Y")]["students"] = schoolHours
+      @allHours[log[0].strftime("%m/%d/%Y")]["all"] = totalsum
+    end
+  end
+  def calculateHours1
     ismentor = School.where("lower(name) = ?","mentor").first
     @allHours = Hash.new{|h,k| h[k] = []}
     overallHours = 0
@@ -12,7 +57,7 @@ class StatsController < ApplicationController
       studentsum=0
       mentorsum=0
       totalsum=0
-      
+
       log[1].each do |timelog|
         totalsum = totalsum+timelog.time_logged
         
