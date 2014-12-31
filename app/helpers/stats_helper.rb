5575c02ada3fab603b542192a60a3edc7d9ae56e
Megan Wenzinger <forlornlawngnome@gmail.com>
2014-12-31 14:53:04 -0500

    debugging stats dropdown empty

--- app/helpers/stats_helper.rb
+++ app/helpers/stats_helper.rb
@@ -3,7 +3,7 @@ module StatsHelper
     start = ApplicationHelper.rangeBeginning.year
     years = Hash.new
     
-    puts "Start Year: #{ApplicationHelper.rangeBeginning.year}"
+    puts "Start Year: #{ApplicationHelper.rangeBeginning}"
     puts "End year: #{ApplicationHelper.getStartYear}"
     (start..ApplicationHelper.getStartYear).each do |year|
       years[ApplicationHelper.dateRange(ApplicationHelper.getYearStart(year))] = year
