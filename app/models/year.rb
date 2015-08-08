c9dbf78a99dae70065f1300ae9d87b63d76f06a4
Megan Wenzinger <forlornlawngnome@gmail.com>
2015-08-08 14:57:34 -0400

    move current_year to be on Year

--- app/models/year.rb
+++ app/models/year.rb
@@ -2,12 +2,12 @@ class Year < ActiveRecord::Base
   attr_accessible :build_season_start, :year_end, :year_start
   
   def is_current_year?
-    if !current_year.nil? && current_year.id == self.id
+    if !Year.current_year.nil? && Year.current_year.id == self.id
       return true
     end
     return false
   end
-  def current_year
+  def self.current_year
     Year.where("year_start <= ? and year_end >= ?",Date.today,Date.today).first
   end
 end
