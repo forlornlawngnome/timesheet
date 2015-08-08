c05625992bb54e555dbad57447da1116d0a94723
Megan Wenzinger <forlornlawngnome@gmail.com>
2015-08-08 17:01:12 -0400

    debugging

--- app/models/year.rb
+++ app/models/year.rb
@@ -14,6 +14,7 @@ class Year < ActiveRecord::Base
     Year.where("year_start <= ? and year_end >= ?",Date.today,Date.today).first
   end
   def self.find_year(date)
+    raise "Here"
     Year.where("year_start <= ? and year_end >= ?",date,date).first
   end
   def year_range
