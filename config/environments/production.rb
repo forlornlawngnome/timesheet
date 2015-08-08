ce346e5457773179ef36228bcafac99fce6318ae
Megan Wenzinger <forlornlawngnome@gmail.com>
2015-08-08 17:39:38 -0400

    debugging

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,7 +4,7 @@ Timesheet::Application.configure do
 
 
   # Code is not reloaded between requests
-  config.cache_classes = true
+  config.cache_classes = false
 
   # Full error reports are disabled and caching is turned on
   config.consider_all_requests_local       = false
