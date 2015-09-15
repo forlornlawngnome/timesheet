f37adf67ad56afe94943376285e356379e6768ab
Megan Wenzinger <forlornlawngnome@gmail.com>
2015-09-15 17:31:11 -0400

    remove utc

--- app/models/timelog.rb
+++ app/models/timelog.rb
@@ -11,7 +11,7 @@ class Timelog < ActiveRecord::Base
   
   ##It views today as the current day UNLESS it is before 1am. If it is before 1am, then it defaults to the previous day as "today"
   scope :in_today, where("timein >= ? AND timeout IS NULL",ApplicationHelper.today.utc)
-  scope :today, where("timein >= ? and timein<= ?",ApplicationHelper.today.utc, ApplicationHelper.today.end_of_day.utc).order("updated_at DESC")
+  scope :today, where("timein >= ? and timein<= ?",ApplicationHelper.today, ApplicationHelper.today.end_of_day).order("updated_at DESC")
 
   
   private
