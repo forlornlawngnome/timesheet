89a366ca2254585a956c1e5475e5d62a67e1724f
Megan Wenzinger <forlornlawngnome@gmail.com>
2016-01-01 23:11:51 -0500

    add so on a users requiremr's requirements page, it will show an * if there is a reason marked. Admins can only see reason, student leaders willh ave to ask for info

--- app/models/hour_exception.rb
+++ app/models/hour_exception.rb
@@ -4,4 +4,6 @@ class HourException < ActiveRecord::Base
   #attr_accessible :date_applicable, :date_sent, :reason, :submitter, :year, :year_id, :user, :user_id
   
   validates_uniqueness_of :user_id, :scope => :date_applicable
+  
+  scope :in_range, -> (date_start, date_end){where("date_applicable >= ? and date_applicable <= ?",date_start,date_end)}
 end
