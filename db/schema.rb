19673a0130f1eb6d705ddf449737d122aeed6642
Megan Wenzinger <forlornlawngnome@gmail.com>
2016-12-23 10:08:32 -0800

    updated so booleans can't be null

--- db/schema.rb
+++ db/schema.rb
@@ -11,17 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161221170806) do
+ActiveRecord::Schema.define(version: 20161222212738) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "forms", force: :cascade do |t|
     t.string   "name",       limit: 255
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
-    t.boolean  "archive",    default: false, null: false
-    t.boolean  "required",   default: false, null: false
+    t.boolean  "archive",                default: false, null: false
+    t.datetime "created_at",                             null: false
+    t.datetime "updated_at",                             null: false
+    t.boolean  "required",               default: false, null: false
   end
 
   create_table "forms_users", force: :cascade do |t|
@@ -39,10 +39,10 @@ ActiveRecord::Schema.define(version: 20161221170806) do
     t.date     "date_sent"
     t.string   "reason",          limit: 255
     t.integer  "year_id"
-    t.datetime "created_at",                  null: false
-    t.datetime "updated_at",                  null: false
+    t.datetime "created_at",                                  null: false
+    t.datetime "updated_at",                                  null: false
     t.integer  "week_id"
-    t.boolean  "made_up_hours",   default: false, null: false
+    t.boolean  "made_up_hours",               default: false, null: false
   end
 
   add_index "hour_exceptions", ["user_id"], name: "index_hour_exceptions_on_user_id", using: :btree
@@ -105,8 +105,8 @@ ActiveRecord::Schema.define(version: 20161221170806) do
   add_index "timelogs", ["user_id"], name: "index_timelogs_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  limit: 255, default: "", null: false
-    t.string   "encrypted_password",     limit: 255, default: "", null: false
+    t.string   "email",                  limit: 255, default: "",    null: false
+    t.string   "encrypted_password",     limit: 255, default: "",    null: false
     t.string   "reset_password_token",   limit: 255
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -115,23 +115,23 @@ ActiveRecord::Schema.define(version: 20161221170806) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip",     limit: 255
     t.string   "last_sign_in_ip",        limit: 255
-    t.datetime "created_at",                                      null: false
-    t.datetime "updated_at",                                      null: false
+    t.datetime "created_at",                                         null: false
+    t.datetime "updated_at",                                         null: false
+    t.boolean  "admin",                              default: false, null: false
     t.string   "phone",                  limit: 255
     t.string   "name_first",             limit: 255
     t.string   "name_last",              limit: 255
     t.string   "userid",                 limit: 255
-    t.boolean  "admin",                  default: false, null: false
     t.integer  "school_id"
     t.boolean  "tools"
     t.boolean  "conduct"
     t.boolean  "basicSafety"
     t.string   "password_salt",          limit: 255
     t.string   "password_hash",          limit: 255
+    t.boolean  "archive",                            default: false, null: false
     t.string   "location",               limit: 255
     t.string   "gender",                 limit: 255
     t.string   "graduation_year",        limit: 255
-    t.boolean  "archive",                default: false, null: false
     t.boolean  "student_leader"
   end
 
